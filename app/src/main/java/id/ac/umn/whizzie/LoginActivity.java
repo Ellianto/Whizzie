37f79ae91bb241e4ac66d72664c706e3ebc62fdb
Felicia Karissa <feliciakarissa@gmail.com>
2019-04-19 00:18:23 +0700

    Logo Cross

--- app/src/main/java/id/ac/umn/whizzie/LoginActivity.java
+++ app/src/main/java/id/ac/umn/whizzie/LoginActivity.java
@@ -15,8 +15,6 @@ public class LoginActivity extends AppCompatActivity {
         Button loginButton = findViewById(R.id.btnLoggingIn);
         Button signUpButton = findViewById(R.id.btnMoveToSignUp);
 
-
-
         final IntentMovement intentMovement = new IntentMovement(LoginActivity.this);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
