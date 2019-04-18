8a4479ac157d70d1bb5ed92914eb03dc32167c01
Felicia Karissa <feliciakarissa@gmail.com>
2019-04-18 21:33:56 +0700

    Sudah bisa slide ke kanan dan kiri untuk sign up dan sign in fragment.

--- app/src/main/java/id/ac/umn/whizzie/SignInFragment.java
+++ app/src/main/java/id/ac/umn/whizzie/SignInFragment.java
@@ -49,6 +49,7 @@ public class SignInFragment extends Fragment {
 
             private void setFragment(Fragment fragment) {
                 FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(R.anim.slide_from_right,R.anim.slideout_from_left);
                 fragmentTransaction.replace(parentFrameLayout.getId(), fragment);
                 fragmentTransaction.commit();
             }
